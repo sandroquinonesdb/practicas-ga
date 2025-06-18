c0ef3e9a2ec2b2887cd4ac0f87792ab1911d3c0a
Fernando <fernando93d@gmail.com>
2025-06-17 18:40:23 -0600

    Wip

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,16 @@
+BRANCH=$1
+
+if [ "$BRANCH" == "main" ]; then
+  DEPLOY_PATH="/home/azureuser/practicas-ga"
+else
+  DEPLOY_PATH="/home/azureuser/stage"
+fi
+
+
 ssh -o StrictHostKeyChecking=no azureuser@4.227.181.213 <<HTML
 
-  cd /home/azureuser/practicas-ga
+  cd $DEPLOY_PATH
 
-  git pull --rebase origin main
+  git pull --rebase origin $BRANCH
 
 HTML
